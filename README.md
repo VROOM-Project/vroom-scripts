d460dcb87baf63039dcf01dc6f24d653d5439ef1
jcoupey <git@coupey.fr>
2017-05-19 15:30:10 +0200

    Generate problem from all nodes with given amenity in a bounding box.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 A bunch of scripts to develop, benchmark, debug or help in using VROOM.
 
+- **overpass_to_json** generate a problem from all OSM nodes with
+  given amenity in a bounding box.
 - **random_problem** generates ready-to-solve random problem based on
   a bounding box.
 - **global_indicators** collects indicators on all solutions contained
---
+++ src/overpass_to_json.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse, sys
+from utils.format_input import write_files
+from utils.overpass import amenity_coordinates_bb
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Generate problem from an overpass query')
+  parser.add_argument('-a', '--amenity', metavar = 'AMENITY',
+                      help = 'tag value to use in overpass query',
+                      default = "cafe")
+  parser.add_argument('-o', '--output', metavar = 'OUTPUT',
+                      help = 'output file name',
+                      default = None)
+  parser.add_argument('--top', metavar = 'TOP',
+                      help = 'bounding box max latitude',
+                      type = float,
+                      default = 48.86),
+  parser.add_argument('--bottom', metavar = 'BOTTOM',
+                      help = 'bounding box min latitude',
+                      type = float,
+                      default = 48.85),
+  parser.add_argument('--left', metavar = 'LEFT',
+                      help = 'bounding box min longitude',
+                      type = float,
+                      default = 2.37),
+  parser.add_argument('--right', metavar = 'RIGHT',
+                      help = 'bounding box max longitude',
+                      type = float,
+                      default = 2.39),
+  parser.add_argument('--geojson', action='store_true',
+                      help = 'also write a geojson file with all generated points',
+                      default = False)
+
+  args = parser.parse_args()
+
+  file_name = args.output;
+  if not file_name:
+    file_name = args.amenity + '_' + str(args.bottom) + '_' + str(args.left) + '_' + str(args.top) + '_' + str(args.right)
+
+  nodes = amenity_coordinates_bb(args.amenity,
+                                 [[args.left, args.bottom],
+                                  [args.right, args.top]])['elements']
+
+  if len(nodes) < 2:
+    print "Too few nodes to format a problem!"
+    sys.exit(0)
+
+  lons = map(lambda n: n['lon'], nodes)
+  lats = map(lambda n: n['lat'], nodes)
+
+  write_files(file_name, lons, lats, args.geojson)
