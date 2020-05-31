f4ae5793722f8d0564a5d2349930e4f856fe1c04
jcoupey <git@coupey.fr>
2020-05-31 17:47:26 +0200

    Add vehicle max load and ratio to capacity.

--- src/vehicle_stats.py
+++ src/vehicle_stats.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-import json, sys, numpy
+import json, sys
+import numpy as np
 
 # Report simple stats on vehicle usage in a solution.
+
+def s_round(v, d):
+  if d == 0:
+    return str(int(v))
+  else:
+    return str(round(v, d))
 
 def generate_stats(input_file, sol_file):
   with open(input_file, 'r') as data:
@@ -10,7 +17,26 @@ def generate_stats(input_file, sol_file):
   with open(sol_file, 'r') as data:
     solution = json.load(data)
 
-  print(','.join(["vehicle", "start", "end", "working_time", "service_rate", "travel_rate", "waiting_rate", "working_ratio"]))
+  amount_size = len(solution['summary']['delivery'])
+
+  headers = [
+    "vehicle",
+    "start",
+    "end",
+    "working_time",
+    "service_rate",
+    "travel_rate",
+    "waiting_rate",
+    "working_ratio",
+    "max_load",
+  ]
+  if (amount_size > 1):
+    headers += (amount_size - 1) * [""]
+  headers.append("max_load_ratio")
+  if (amount_size > 1):
+    headers += (amount_size - 1) * [""]
+
+  print(','.join(headers))
 
   starts = []
   ends = []
@@ -19,6 +45,8 @@ def generate_stats(input_file, sol_file):
   travel_rates = []
   waiting_rates = []
   working_ratios  = []
+  max_loads  = []
+  max_load_ratios  = []
 
   for route in solution['routes']:
     v_id = route['vehicle']
@@ -54,12 +82,48 @@ def generate_stats(input_file, sol_file):
     if ('time_window' in v):
       available_time = v['time_window'][1] - v['time_window'][0]
       working_ratio = 100 * float(working_time) / available_time
-      current.append(str(round(working_ratio, 1)))
+      current.append(s_round(working_ratio, 1))
       working_ratios.append(working_ratio)
     else:
       current.append("")
 
+    if (amount_size > 0):
+      max_load = amount_size * [0]
+      for step in route['steps']:
+        for i in range(amount_size):
+          if (max_load[i] < step['load'][i]):
+            max_load[i] = step['load'][i]
+      current += map(lambda l: str(l), max_load)
+      max_loads.append(max_load)
+
+      max_load_ratio = []
+      for i in range(amount_size):
+        max_load_ratio.append(100 * float(max_load[i]) / v['capacity'][i])
+
+      current += map(lambda r: s_round(r, 1), max_load_ratio)
+      max_load_ratios.append(max_load_ratio)
+
     print(','.join(current))
+
+  print(',')
+  averages = [
+    'Average',
+    s_round(np.mean(starts), 0),
+    s_round(np.mean(ends), 0),
+    s_round(np.mean(working_time), 0),
+    s_round(np.mean(service_rate), 1),
+    s_round(np.mean(travel_rate), 1),
+    s_round(np.mean(waiting_rate), 1),
+    s_round(np.mean(working_ratio), 1)
+  ]
+
+  if (amount_size > 0):
+    for i in range(amount_size):
+      averages.append(s_round(np.mean([load[i] for load in max_loads]), 0))
+    for i in range(amount_size):
+      averages.append(s_round(np.mean([load[i] for load in max_load_ratios]), 1))
+
+  print(','.join(averages))
 
 if __name__ == "__main__":
   # Arguments are the name of the input and solution files.
