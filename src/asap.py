5ec07c7858d91ac0b055d895051e0fc27e41afd0
jcoupey <git@coupey.fr>
2021-08-25 16:34:42 +0200

    Plot pareto front for completion/cost.

--- src/asap.py
+++ src/asap.py
@@ -3,6 +3,7 @@
 import copy
 import json
 import sys
+import matplotlib.pyplot as plt
 from utils.file import load_json
 from utils.vroom import solve
 
@@ -86,6 +87,40 @@ def dichotomy(data, first_solution):
     return solutions
 
 
+def plot_pareto_front(indicators, pareto_file, full_Y_scale=False):
+    fig, ax1 = plt.subplots(1, 1)
+    plt.xlabel("Completion time")
+    plt.ylabel("Cost")
+
+    options = {
+        "dichotomy": {"marker": "^", "edgecolor": "red", "linewidth": 0.7},
+        "backward_search": {"marker": "o", "edgecolor": "blue", "linewidth": 0.5},
+    }
+
+    ymax = indicators[0]["cost"]
+
+    for origin in ["backward_search", "dichotomy"]:
+        costs = [i["cost"] for i in indicators if i["origin"] == origin]
+        completions = [i["completion"] for i in indicators if i["origin"] == origin]
+        ymax = max(ymax, max(costs))
+
+        ax1.scatter(
+            completions,
+            costs,
+            facecolor="none",
+            edgecolor=options[origin]["edgecolor"],
+            marker=options[origin]["marker"],
+            linewidth=options[origin]["linewidth"],
+        )
+
+    if full_Y_scale:
+        ax1.set_ylim(0, ymax * 1.05)
+
+    plt.savefig(pareto_file, bbox_inches="tight")
+    plt.show()
+    plt.close()
+
+
 def backward_search(data, first_solution):
     current = copy.deepcopy(data)
     current_sol = copy.deepcopy(first_solution)
@@ -127,7 +162,7 @@ def backward_search(data, first_solution):
     return solutions
 
 
-def solve_asap(data):
+def solve_asap(data, pareto_file=""):
     init_solution = solve(data)
 
     if init_solution["code"] != 0:
@@ -152,6 +187,8 @@ def solve_asap(data):
         for sol in solutions
     ]
 
+    plot_pareto_front(indicators, pareto_file)
+
     # Print solution with smallest completion time to standard output.
     best_rank = 0
     smallest_completion = indicators[0]["completion"]
@@ -167,4 +204,6 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
 
     data = load_json(input_file)
-    solve_asap(data)
+
+    pareto_file = input_file[: input_file.rfind(".json")] + "_pareto.svg"
+    solve_asap(data, pareto_file)
