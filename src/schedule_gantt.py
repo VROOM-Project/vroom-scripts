119ed1a115529c3fa619b53dac0da2ab3fbf4717
aheam <arthur.heam@laposte.net>
2022-05-23 10:20:56 +0200

    black + flake8

--- src/schedule_gantt.py
+++ src/schedule_gantt.py
@@ -1,10 +1,8 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import json
-from xmlrpc.client import MAXINT
 import matplotlib.pyplot as plt
 import matplotlib.colors as clrs
 import sys
-import numpy as np
 
 # Very simple plot for a VROOM solution file.
 
@@ -39,6 +37,7 @@ colors_blacklist = [
     "lavenderblush",
 ]
 
+
 def plot_schedules(sol_file_name):
     plot_file_name = sol_file_name[0 : sol_file_name.rfind(".json")] + "_gantt.svg"
 
@@ -50,7 +49,7 @@ def plot_schedules(sol_file_name):
     for name, hex in clrs.cnames.items():
         if name not in colors_blacklist:
             color_list.append(name)
-    
+
     fig, ax1 = plt.subplots(1, 1)
     fig.set_figwidth(15)
     plt.subplots_adjust(left=0.03, right=1, top=1, bottom=0.05, wspace=0.03)
@@ -70,15 +69,37 @@ def plot_schedules(sol_file_name):
             t += dt
             d1, d2 = d2, step["duration"]
             dt = d2 - d1
-            ax1.hlines(y = route["vehicle"], xmin = t, xmax = t + dt, colors = color_list[route["vehicle"] % len(color_list)])
+            ax1.hlines(
+                y=route["vehicle"],
+                xmin=t,
+                xmax=t + dt,
+                colors=color_list[route["vehicle"] % len(color_list)],
+            )
             t += dt
             dt = step["setup"]
-            ax1.hlines(y = route["vehicle"], xmin = t, xmax = t + dt, colors = color_list[route["vehicle"] % len(color_list)], linewidth = 5)
+            ax1.hlines(
+                y=route["vehicle"],
+                xmin=t,
+                xmax=t + dt,
+                colors=color_list[route["vehicle"] % len(color_list)],
+                linewidth=5,
+            )
             t += dt
             dt = step["service"]
             if step["type"] in ["job", "pickup", "delivery"]:
-                ax1.vlines(x = t, ymin = (route["vehicle"])-0.5+5/(n+10), ymax = route["vehicle"]+0.5-5/(n+10), colors = color_list[route["vehicle"] % len(color_list)])
-                ax1.hlines(y = route["vehicle"], xmin = t, xmax = t + dt, colors = color_list[route["vehicle"] % len(color_list)], linewidth = 5)
+                ax1.vlines(
+                    x=t,
+                    ymin=(route["vehicle"]) - 0.5 + 5 / (n + 10),
+                    ymax=route["vehicle"] + 0.5 - 5 / (n + 10),
+                    colors=color_list[route["vehicle"] % len(color_list)],
+                )
+                ax1.hlines(
+                    y=route["vehicle"],
+                    xmin=t,
+                    xmax=t + dt,
+                    colors=color_list[route["vehicle"] % len(color_list)],
+                    linewidth=5,
+                )
             t += dt
 
     computing_time = solution["summary"]["computing_times"]["loading"]
@@ -91,11 +112,12 @@ def plot_schedules(sol_file_name):
     title += " ; computing time: " + str(computing_time)
     title += "ms"
     ax1.set_title(title)
-    
+
     print("Plotting file " + plot_file_name)
     plt.savefig(plot_file_name, bbox_inches="tight")
     plt.close()
     # plt.show()
+
 
 if __name__ == "__main__":
     # Argument is the name of the solution fils to plot.
