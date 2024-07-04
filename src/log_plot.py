3df5e09daf2420319a3e4ddb016f298bed7f1f9c
jcoupey <git@coupey.fr>
2024-07-04 12:49:49 +0200

    Materialize rollbacks and depth levels.

--- src/log_plot.py
+++ src/log_plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import sys
 
+START = "Start"
 JOB_ADDITION = "JobAddition"
 RUIN = "Ruin"
 ROLLBACK = "Rollback"
@@ -20,16 +21,18 @@ def log_plot(log_file):
     fig.set_figwidth(15)
 
     steps = data[0]["steps"]
-    specific_ranks = {"job_addition": [], "ruin": [], "rollback": []}
+    specific_ranks = {"start": [], "job_addition": [], "ruin": [], "rollback": []}
 
     best_score = steps[0]["score"]
     best_score_rank = 0
+
+    min_cost = min([s["score"]["cost"] for s in steps])
+    max_cost = max([s["score"]["cost"] for s in steps])
 
     assigned_values = sorted(set([s["score"]["assigned"] for s in steps]), reverse=True)
     use_colormap = len(assigned_values) != 1
 
     if use_colormap:
-        print(assigned_values)
         max_assigned = max(assigned_values)
         cmap = mpl.cm.viridis
         norm = mpl.colors.BoundaryNorm(
@@ -72,6 +75,8 @@ def log_plot(log_file):
             continue
 
         # Moving on into current LS step.
+        if event == START:
+            specific_ranks["start"].append(i)
         if event == JOB_ADDITION:
             specific_ranks["job_addition"].append(i)
 
@@ -84,7 +89,7 @@ def log_plot(log_file):
         )
         plot_colors.append(current_color)
 
-    # Materialize job addition events
+    # Materialize job addition events.
     for i in specific_ranks["job_addition"]:
         time = steps[i]["time"]
         ax1.plot(
@@ -95,6 +100,25 @@ def log_plot(log_file):
             ],
             color="gray",
             linewidth=0.5,
+        )
+
+    # Materialize rollback events and depth levels.
+    for i in specific_ranks["rollback"]:
+        time = steps[i]["time"]
+        ax1.plot(
+            [time],
+            [steps[i]["score"]["cost"]],
+            "o",
+            ms=6,
+            markerfacecolor="None",
+            markeredgecolor="black",
+            markeredgewidth=0.8,
+        )
+        ax1.plot(
+            [time, time],
+            [min_cost, max_cost],
+            color="black",
+            linewidth=1,
         )
 
     ax1.scatter(plot_times, plot_costs, s=4, c=plot_colors, linewidths=0)
@@ -111,10 +135,10 @@ def log_plot(log_file):
         [steps[best_score_rank]["time"]],
         [best_score["cost"]],
         "o",
-        ms=10,
+        ms=12,
         markerfacecolor="None",
         markeredgecolor="green",
-        markeredgewidth=1,
+        markeredgewidth=1.2,
     )
 
     # print("Plotting file " + log_plot_name)
