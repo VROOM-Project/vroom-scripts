727bee66c7f11dbe7e1a9653087b40c25accd725
jcoupey <git@coupey.fr>
2025-02-13 13:43:37 +0100

    Optional plot routes bounding boxes.

--- src/plot.py
+++ src/plot.py
@@ -48,10 +48,19 @@ def plot_routes(solution, plot_base_name):
 
         ax1.plot(lons, lats, color=color_list[route["vehicle"] % len(color_list)])
 
-        xmin = min(xmin, min(lons))
-        xmax = max(xmax, max(lons))
-        ymin = min(ymin, min(lats))
-        ymax = max(ymax, max(lats))
+        bbox = [[min(lons), min(lats)], [max(lons), max(lats)]]
+
+        xmin = min(xmin, bbox[0][0])
+        xmax = max(xmax, bbox[1][0])
+        ymin = min(ymin, bbox[0][1])
+        ymax = max(ymax, bbox[1][1])
+
+        # ax1.plot(
+        #     [bbox[0][0], bbox[1][0], bbox[1][0], bbox[0][0], bbox[0][0]],
+        #     [bbox[0][1], bbox[0][1], bbox[1][1], bbox[1][1], bbox[0][1]],
+        #     linestyle='dotted',
+        #     color=color_list[route["vehicle"] % len(color_list)],
+        # )
 
         step = route["steps"][-1]
         if step["type"] == "end":
